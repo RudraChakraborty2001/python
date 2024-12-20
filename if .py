90979abe8d7b8eaa0588d090c08a660478e8fded
Rudra Chakraborty <crudra630@gmail.com>
2024-12-20 09:59:58 +0530

    reverting comments

--- if .py
+++ if .py
@@ -6,7 +6,7 @@ if age>=100:
      print("You are too old to sign up !")
 elif age<0:
     print("You haven't been born yet!")     
-# elif age>=18:
-#     print("Your are now signed up !")    
+elif age>=18:
+    print("Your are now signed up !")    
 else:
      print("You must be 18+ to sign up!")    
--- while-loop.py
+++ while-loop.py
@@ -2,4 +2,4 @@ name = input("Enter your name: ")
 while name == "":
     print("You did not enter your name")
     name = input("Enter your name: ")
-# print(f"Hello  {name}")   
+print(f"Hello {name}")  
