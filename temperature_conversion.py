7a4738ca9c7f9503bb1080fb32642536e7a59762
Rudra Chakraborty <crudra630@gmail.com>
2025-01-15 17:16:46 +0530

    DEFAULT ARGUMENTS,ITERABLES ,keyword argument
    DEFAULT ARGUMENTS,ITERABLES ,keyword argument

---
+++ temperature_conversion.py
@@ -0,0 +1,13 @@
+# ####......Temperature conversion program......####
+
+# unit = input("Is this temperatue in Celsius or fahrenheit (C/F): ")
+# temp = float(input("Ente the temperature: "))
+
+# if unit=="C":
+#     temp=round((temp*9)/5+32, 1)# Celsius to fahrenheit "C*9/5+31 = °F"
+#     print(f"The temperatue in Fahrenheit is : {temp}°F")# for "°" (type Alt+0176) 
+# elif unit=="F":
+#     temp=round((temp-32)*5/9 ,1)#fahrenheit to Celsius(F°-32)*5/9 = °C   
+#     print(f"The temperatue in Celsius is : {temp}°C")
+# else:
+#     print(f"{unit} is an invalid unit of measurement")
